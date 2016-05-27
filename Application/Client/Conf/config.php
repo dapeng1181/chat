2cd115a7288148545c5379362b5b232329efb6af
1528065095@qq.com <1528065095@qq.com>
2016-05-27 23:24:34 +0800

    更新

--- Application/Client/Conf/config.php
+++ Application/Client/Conf/config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
     'OFFICIAL_WEB' => 'http://www.duiler.com',
-	'ClIENT_HOST' => 'http://localhost',
-    'CLIENT_WS' => 'ws://localhost:8585'
+	'ClIENT_HOST' => 'http://www.duiler.com',
+    'CLIENT_WS' => 'ws://www.duiler.com:8585'
 );
